91c68fb8b620ed79bc4bc43dc521e0a3ea97e4e7
Donna Dietz <donna.dietz@gmail.com>
2019-09-04 08:41:35 -0400

    Initial commit

---
+++ hw1_f19.py
@@ -0,0 +1,64 @@
+'''This is Homework One for CSC 280 - 005 Fall 2019.
+This will be distributed through Github Classroom.
+Please fill in your name, and make sure your
+code is executable. Each answer should show up properly
+when I run the code while grading.  There are sample
+exercises in the code for the purpose of showing you
+the format I'm looking for in your responses.
+'''
+
+# PROBLEM ONE: Create variables for each data type listed, and print as a demo.
+print("")
+print("------------------- Homework One: ------------------")
+myname = "PUT YOUR NAME HERE"
+print("My name is: ", myname)
+print ("Problem one: ..... ..... ..... .....")
+
+# Boolean
+b = True
+print(b, "is type", type(b))
+
+
+# List
+
+# integer
+
+# float
+
+# string
+
+
+
+# PROBLEM TWO: Show the use of each of: +, -, *, **, /, //, % with numbers.
+print("")
+print ("Problem two: ..... ..... ..... .....")
+
+a,b =5,9
+
+print(a, '+', b, '=', a+b)  #This demonstrates +
+
+
+
+
+# PROBLEM THREE: Use the range command and other skills to find the
+#                sum of odd integers between 1003 and 30239 inclusive.
+print("")
+print ("Problem three: ..... ..... ..... .....")
+
+# Example: Sum of even integers between 8 and 20 inclusive:
+print("Sum of evens between 8 and 20: ", sum(range(8,21,2)))
+
+
+
+# PROBLEM FOUR: Create a list of 10 strings. Then, print out the third and
+#               seventh items.
+print("")
+print ("Problem four: ..... ..... ..... .....")
+
+
+#Example: Here, I create a list of 5 integers and print the 4th one.
+L=[3,5,2,0,1]
+print("My list: ",L)
+print("The fourth item in my list is: ", L[3])
+
+
