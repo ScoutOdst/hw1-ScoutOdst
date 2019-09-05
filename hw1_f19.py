f64d07227c4e83848d97863d54fda61e2e5b7572
mbn12 <jr4252a@student.american.edu>
2019-09-05 09:16:42 -0400

    uu

--- hw1_f19.py
+++ hw1_f19.py
@@ -8,7 +8,7 @@ the format I'm looking for in your responses.
 '''
 
 # PROBLEM ONE: Create variables for each data type listed, and print as a demo.
-print("")
+print("")ff
 print("------------------- Homework One: ------------------")
 myname = "PUT YOUR NAME HERE"
 print("My name is: ", myname)
